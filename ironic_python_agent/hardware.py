a95a8c79c7437285ab8446dc8b372a8238beb1f5
Adam Rozman <adam.rozman@est.tech>
2025-02-18 10:25:09 +0200

    enable nvram/uefi cleanup by default
    This commit:
     - Forces the execution of the 'clean_uefi_nvram' deployment step for every
       deployment.
    
    Reason: When Ironic is used with BMO there is no possibility to change
    the priority of the 'clean_uefi_nvram' deployment step.
    
    Change-Id: Ib43275dd3ee8a4c1eeb0a0f73668bb8ab6d88190
    Signed-off-by: Adam Rozman <adam.rozman@est.tech>

--- ironic_python_agent/hardware.py
+++ ironic_python_agent/hardware.py
@@ -2859,7 +2859,7 @@ class GenericHardwareManager(HardwareManager):
             },
             {
                 'step': 'clean_uefi_nvram',
-                'priority': 0,
+                'priority': 81,
                 'interface': 'deploy',
                 'reboot_requested': False,
                 'argsinfo': DEPLOY_CLEAN_UEFI_NVRAM_ARGSINFO,
